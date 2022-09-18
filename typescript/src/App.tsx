c8e1a8b0b170677bd116ffa49761a228fa35bd8c
choyeon <cho1428hee@naver.com>
2022-09-19 00:20:09 +0900

    3.6절 커밋

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,11 +1,22 @@
-import Circle from "./Circle";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: ${(props)=>props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props)=>props.theme.textColor};
+`;
+
 
 function App() {
+
+
   return (
-    <div>
-      <Circle borderColor="yellow" bgColor="teal"/>
-      <Circle bgColor="tomato"/>
-    </div>
+    <Container>
+      <H1>protected</H1>
+    </Container>
   );
 }
 
