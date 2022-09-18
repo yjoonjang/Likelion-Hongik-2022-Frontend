2e620f69b4d4f9edcfce896c5123290f3cc51a0c
choyeon <cho1428hee@naver.com>
2022-09-19 00:34:13 +0900

    3장 recap

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -9,13 +9,24 @@ const H1 = styled.h1`
   color: ${(props)=>props.theme.textColor};
 `;
 
+interface DummyProps{
+  text:string;
+  active?: boolean;
+}
+
+function Dummy({text, active=false}:DummyProps){
+  return <H1>{text}</H1>
+}
 
 function App() {
-
-
+  const onClick=(event:React.MouseEvent<HTMLButtonElement>)=>{};
   return (
     <Container>
-      <H1>protected</H1>
+      <Dummy text="Hello"/>
+      <form>
+        <button onClick={onClick}>click me</button>        
+      </form>
+
     </Container>
   );
 }
