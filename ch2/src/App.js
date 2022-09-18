72d6106641f0db2729280b82bd6efe9d8a4bd289
choyeon <cho1428hee@naver.com>
2022-09-18 16:00:01 +0900

    ch2.6 commit

---
+++ ch2/src/App.js
@@ -0,0 +1,50 @@
+import styled, {keyframes} from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const rotationAnimation = keyframes`
+  0%{
+    transform: rotate(0deg);
+    border-radius:0px;
+  }
+  50%{
+    border-radius: 100px;
+  }
+  100%{
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
+`;
+
+const Emoji = styled.span`
+  font-size: 36px;
+`;
+
+
+const Box=styled.div`
+  height:200px;
+  width:200px;
+  background-color: tomato;
+  animation:${rotationAnimation} 1s linear infinite;
+  display:flex;
+  justify-content:center;
+  align-items: center;
+  ${Emoji}:hover{
+      font-size: 98px;
+  }
+`;
+
+function App() {
+  return (
+    <Wrapper>
+      <Box>
+        <Emoji>🤩</Emoji>
+      </Box>
+      <Emoji>🔥</Emoji>
+    </Wrapper>
+  );
+}
+
+export default App;
