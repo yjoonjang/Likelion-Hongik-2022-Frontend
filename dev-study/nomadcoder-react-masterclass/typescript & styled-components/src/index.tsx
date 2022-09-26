d9b1fdb2095e790133a4d4b06b9139c0e915e6b5
yjoonjang <yjoonjang34@gmail.com>
2022-09-26 14:13:44 +0900

    first commit

---
+++ dev-study/nomadcoder-react-masterclass/typescript & styled-components/src/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+import App from './App';
+
+const darkTheme = {
+  textColor: 'whitesmoke',
+  backgroundColor: '#111',
+};
+
+const lightTheme = {
+  textColor: '#111',
+  backgroundColor: 'whitesmoke',
+};
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
