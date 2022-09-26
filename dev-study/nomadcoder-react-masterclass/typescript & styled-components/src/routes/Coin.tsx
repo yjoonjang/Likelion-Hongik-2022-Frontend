d2f0d15c75327c748b797db5b02f6d9962d3e259
yjoonjang <yjoonjang34@gmail.com>
2022-09-26 18:15:43 +0900

    [#4.5] Fetching info and priceInfo Data

--- dev-study/nomadcoder-react-masterclass/typescript & styled-components/src/routes/Coin.tsx
+++ dev-study/nomadcoder-react-masterclass/typescript & styled-components/src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams, useLocation } from 'react-router';
 import styled from 'styled-components';
 // interface RouteParams {
@@ -33,9 +33,24 @@ function Coin() {
 	const [loading, setLoading] = useState(true);
 	const location = useLocation();
 	const name = location.state as RouterState;
+	const [info, setInfo] = useState({});
+	const [priceInfo, setPriceInfo] = useState({});
 
 	// home을 먼저 들러야 state가 나올 수 있음
 
+	useEffect(() => {
+		(async () => {
+			const infoData = await (
+				await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+			).json();
+			// console.log(res);
+			const priceData = await (
+				await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+			).json();
+			setInfo(infoData);
+			setPriceInfo(priceData);
+		})();
+	}, []);
 	/* 
   강의와는 다르게 useParams 뒤에 타입을 안붙여줘도 됨. 
   react-router-dom v6 이상부터는 useParams 쓰는 순간 자동으로 타입이 sting 아니면 undefined로 결정됨.
