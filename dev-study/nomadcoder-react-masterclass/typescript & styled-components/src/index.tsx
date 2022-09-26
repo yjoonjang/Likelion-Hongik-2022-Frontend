c75d8ee2d379f6770f358b4cf9828deb5009e6c0
yjoonjang <yjoonjang34@gmail.com>
2022-09-26 14:58:10 +0900

    [#4.1] style

--- dev-study/nomadcoder-react-masterclass/typescript & styled-components/src/index.tsx
+++ dev-study/nomadcoder-react-masterclass/typescript & styled-components/src/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { ThemeProvider } from 'styled-components';
-import App from './App';
-import {theme} from './theme'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import { theme } from "./theme";
 
-const container: any = document.getElementById('root')
+const container: any = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
@@ -12,5 +12,5 @@ root.render(
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
