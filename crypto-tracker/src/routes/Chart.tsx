0c277500d3591c6934fac3f0dd90f80f214e515b
Chaeeun Lim <limce21@naver.com>
2022-09-27 23:55:33 +0900

    [ADD] 4.12 Price Chart

--- crypto-tracker/src/routes/Chart.tsx
+++ crypto-tracker/src/routes/Chart.tsx
@@ -1,4 +1,12 @@
-function Chart() {
+import { useQuery } from 'react-query';
+import { fetchCoinHistory } from '../api';
+
+interface CharProps {
+	coinId: string;
+}
+
+function Chart({ coinId }: CharProps) {
+	const { isLoading, data } = useQuery(['ohlcv', coinId], () => fetchCoinHistory(coinId));
 	return <h1>Chart</h1>;
 }
 
--- crypto-tracker/src/routes/Coin.tsx
+++ crypto-tracker/src/routes/Coin.tsx
@@ -208,7 +208,7 @@ function Coin() {
 							<Price />
 						</Route>
 						<Route path={`/:coinId/chart`}>
-							<Chart />
+							<Chart coinId={coinId} />
 						</Route>
 					</Switch>
 				</>
