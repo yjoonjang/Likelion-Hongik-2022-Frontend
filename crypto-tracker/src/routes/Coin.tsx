7c976ae52d8509f499278353aad3fa4d59edf2ba
Chaeeun Lim <limce21@naver.com>
2022-09-28 00:55:36 +0900

    [ADD] 4.15 Final Touches

--- crypto-tracker/src/routes/Coin.tsx
+++ crypto-tracker/src/routes/Coin.tsx
@@ -5,6 +5,7 @@ import Price from './Price';
 import Chart from './Chart';
 import { fetchCoinInfo, fetchCoinTickers } from '../api';
 import { useQuery } from 'react-query';
+import { Helmet } from 'react-helmet';
 
 interface RouteParams {
 	coinId: string;
@@ -142,7 +143,7 @@ function Coin() {
 	const priceMatch = useRouteMatch('/:coinId/price');
 	const chartMatch = useRouteMatch('/:coinId/chart');
 	const { isLoading: infoLoading, data: infoData } = useQuery<InfoData>(['info', coinId], () => fetchCoinInfo(coinId));
-	const { isLoading: tickersLoading, data: tickersData } = useQuery<PriceData>(['tickers', coinId], () => fetchCoinTickers(coinId));
+	const { isLoading: tickersLoading, data: tickersData } = useQuery<PriceData>(['tickers', coinId], () => fetchCoinTickers(coinId), { refetchInterval: 5000 });
 	// const [loading, setLoading] = useState(true);
 	// const [info, setInfo] = useState<InfoData>();
 	// const [priceInfo, setPriceInfo] = useState<PriceData>();
@@ -161,6 +162,9 @@ function Coin() {
 	const loading = infoLoading || tickersLoading;
 	return (
 		<Container>
+			<Helmet>
+				<title>{state?.name ? state.name : loading ? 'Loading...' : infoData?.name}</title>
+			</Helmet>
 			<Header>
 				<Title>{state?.name ? state.name : loading ? 'Loading...' : infoData?.name}</Title>
 			</Header>
@@ -178,8 +182,8 @@ function Coin() {
 							<span>${infoData?.symbol}</span>
 						</OverviewItem>
 						<OverviewItem>
-							<span>Open Source:</span>
-							<span>{infoData?.open_source ? 'Yes' : 'No'}</span>
+							<span>Price:</span>
+							<span>{tickersData?.quotes.USD.price.toFixed(3)}</span>
 						</OverviewItem>
 					</Overview>
 					<Description>{infoData?.description}</Description>
