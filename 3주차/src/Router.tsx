74afd5f751b8b749382e1b98ef74ff000349dc82
ch9eri <limch9eri@gmail.com>
2022-10-03 23:04:27 +0900

    [#5.0-5.4]  크립토 트래커 클론 + Recoil

---
+++ 3주차/src/Router.tsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Coins from './routes/Coins';
+import Coin from './routes/Coin';
+
+interface IRouterProps {
+
+}
+
+function Router({}:IRouterProps) {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Coins  />} />
+        <Route path="/:coinId/*" element={<Coin  />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
---
+++ 3주차/src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components"
+
+export const darkTheme: DefaultTheme = {
+    bgColor:'black',
+    textColor:'#fbc531',
+    accentColor:'violet',
+}
+
+export const lightTheme: DefaultTheme = {
+    bgColor:'whitesmoke',
+    textColor:'black',
+    accentColor:'#fbc531',
+}
